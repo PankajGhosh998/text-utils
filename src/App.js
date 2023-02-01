a72aca0c9f6187ff8c69ace7829174a725a8ed94
PankajGhosh998 <Pankajghosh998@gmail.com>
2023-02-01 16:28:19 +0530

    added all component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,70 @@
+import React from "react";
 import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Textarea from "./components/Textarea";
-// import About from './components/About'
+import Alert from "./components/Alert";
+import About from "./components/About";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (massage, type) => {
+    setAlert({ msg: massage, type: type });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
 
   const modeChangeHandler = () => {
     if (mode === "light") {
       setMode("dark");
       document.body.style.backgroundColor = "#343a40";
       document.body.style.color = "white";
+      showAlert("Darkmode on", "success");
     } else {
       setMode("light");
       document.body.style.backgroundColor = "white";
       document.body.style.color = "black";
+      showAlert("Darkmode off", "success");
     }
   };
   return (
-    <div>
+    <Router>
       <Navbar
         brandName="TextUtils"
         mode={mode}
         toggleMode={modeChangeHandler}
       />
-      <div className="container my-3">
-        <Textarea
-          heading="Text analyzer"
-          mode={mode}
-          toggleMode={modeChangeHandler}
+      <Alert alert={alert} />
+      <Routes>
+        <Route
+          path="/about"
+          element={
+            <About
+              heading="About Us"
+              mode={mode}
+              toggleMode={modeChangeHandler}
+            />
+          }
         />
-        {/* <About/> */}
-      </div>
-    </div>
+        <Route
+          path="/"
+          element={
+            <div className="container my-3">
+              <Textarea
+                heading="Text analyzer"
+                mode={mode}
+                toggleMode={modeChangeHandler}
+                showAlert={showAlert}
+              />
+            </div>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
