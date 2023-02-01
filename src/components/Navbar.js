fd62114cee26dc626e8d736bc5a8b9ed7a0b93ef
PankajGhosh998 <Pankajghosh998@gmail.com>
2023-02-01 16:27:49 +0530

    create navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 export default function Navbar(props) {
   return (
@@ -8,9 +9,9 @@ export default function Navbar(props) {
         className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
       >
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">
+          <Link className="navbar-brand" to="/">
             {props.brandName}
-          </a>
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
@@ -25,19 +26,14 @@ export default function Navbar(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
+                <Link className="nav-link active" aria-current="page" to="/">
                   {props.tab1}
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/">
+                <Link className="nav-link" to="/about">
                   {props.tab2}
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">
-                  {props.tab3}
-                </a>
+                </Link>
               </li>
             </ul>
             <div className="form-check form-switch my-3">
@@ -66,13 +62,11 @@ export default function Navbar(props) {
 Navbar.prototype = {
   brandName: PropTypes.string.isRequired,
   tab1: PropTypes.string,
-  tab2: PropTypes.string,
-  tab3: PropTypes.string,
+  tab2: PropTypes.string
 };
 
 Navbar.defaultProps = {
   brandName: "Brand Name",
   tab1: "Home",
   tab2: "About",
-  tab3: "Contact",
 };
